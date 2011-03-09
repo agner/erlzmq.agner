1c6e995051ec3b7033889514de53cbd3025ff8e7
Yurii Rashkovskii <yrashk@gmail.com>
2011-03-09 06:32:07 -0800

    Use erlzmq2 as a new (v2) binding

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-cd zeromq2 && ./autogen.sh && ./configure --prefix=`pwd`/.prefix && make && make install && cd ..
-cd erlzmq && ./bootstrap && ./configure --enable-static=yes --with-zeromq=`pwd`/../zeromq2/.prefix && make && cd ..
-rm -rf zeromq2
-mv erlzmq/* .
-rm -rf erlzmq
